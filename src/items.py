cbbadcf0eb0a35e32242436c6b07b5caf058178b
Ben Liu <ben4932042@icloud.com>
2022-09-21 16:18:21 +0800

    feat: add ithome aritcle content crawler

--- src/items.py
+++ src/items.py
@@ -1,14 +1,23 @@
-# pylint: disable=no-member
+# pylint: disable=no-member, no-self-argument
 """ithome extract data item"""
 import datetime
 import pydantic
 
-
-class IthomeContentInfoItem(pydantic.BaseModel):
-    """ithome content data"""
+class ArticleItem(pydantic.BaseModel):
+    """ithome aritcle info"""
+    user_id: int
+    ironman_id: int
     title: str
     like: int = 0
     comment: int = 0
     view: int = 0
-    url: pydantic.HttpUrl
+    article_id: int
+    article_url: pydantic.HttpUrl
     create_datetime: datetime.datetime
+
+class ContentItem(pydantic.BaseModel):
+    """ithome content info"""
+    text: str
+
+class IthomeIronManItem(ArticleItem,ContentItem):
+    """ithome article and content info"""
